c578875e41c8b61d905ffeaacb50c86870562881
rextuus <rextuus@gmail.com>
2020-11-26 01:12:25 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <detector/BuildConfig.h>
 #include <detector/IbFabric.h>
+#include <detector/IbNode.h>
 #include <detector/exception/IbFileException.h>
+#include <csignal>
+#include <chrono>
+#include <thread>
+
+bool isRunning = true;
+
+static void SignalHandler(int signal) {
+    if(signal == SIGINT) {
+        isRunning = false;
+    }
+}
 
 int main() {
     Detector::BuildConfig::printBanner();
     bool network = true;
     bool compatibility = true;
-    Detector::IbFabric *fabric = nullptr;
+    Detector::IbFabric fabric(network, compatibility);
 
-    try {
-        fabric = new Detector::IbFabric(network, compatibility);
-    } catch(const Detector::IbPerfException &exception) {
-        printf("An exception occurred: %s", exception.what());
+    while(isRunning) {
+        try {
+            fabric.RefreshCounters();
+            std::cout << fabric << std::endl << std::endl;
+        } catch(const Detector::IbPerfException &exception) {
+            printf("An exception occurred: %s", exception.what());
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 
     std::cout << "Hello, World!" << std::endl;
