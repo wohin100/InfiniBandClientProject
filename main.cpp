cad2925130e618c9999d7f4340984aacd1cfbb42
rextuus <rextuus@gmail.com>
2021-01-14 19:28:38 +0100

    test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <nlohmann/json.hpp>
 #include <iostream>
+#include <unistd.h>
 #include "Client.h"
 #include "InfinibandReader.h"
 
@@ -11,6 +12,8 @@ using nlohmann::json;
 using namespace std;
 int serverPort = 80;
 char *serverAddress;
+int clientNr = 1;
+
 
 bool isRunning = true;
 
@@ -30,20 +33,22 @@ int main(int argc, char *argv[]) {
     } else {
         serverAddress = argv[1];
         serverPort = stoi(argv[2]);
+        clientNr = stoi(argv[3]);
     }
 
+    int ground = 10;
+    string dataToSend =  "{\"MulticastRcvPkts\":8,\"node\":\"mlx4_0\",\"port\":1,\"received\":"+std::to_string(ground)+",\"transmitted\":33984}";
 
-    string dataToSend = "{ \"happy\": true, \"pi\": 3.141 }";
-    auto j3 = json::parse(dataToSend);
-
-    std::cout << j3["happy"] << "\n";
-
+    while (true) {
     // collect infiniband infos
-    InfinibandReader::collectNodeInfos();
+    json infos = InfinibandReader::collectNodeInfos();
+    string debugOutput = infos.dump();
     auto *client = new Client(serverAddress, serverPort);
 
-    for (int i = 0; i < 10; ++i) {
-        client->sendDataToServer(dataToSend);
+        int randomNumber = rand() % 10;
+        dataToSend =  "{\"MulticastRcvPkts\":8,\"node\":\""+std::to_string(clientNr)+"\",\"port\":1,\"received\":"+std::to_string(randomNumber)+",\"transmitted\":33984}";
+        client->sendDataToServer(debugOutput);
+        usleep(100000);
     }
 
     return 0;
